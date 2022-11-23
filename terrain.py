994f342fbf0a9cd64fefb07d90a3f9468d30ef28
lakshmi-adiga <ladiga@andrew.cmu.edu>
2022-11-22 23:00:16 -0500

    basically undid all my progress

--- terrain.py
+++ terrain.py
@@ -1,14 +1,19 @@
 from cmu_112_graphics import *
 
 def appStarted(app):
-    app.tile = app.loadImage("Images/tiles.png")
+    app.ogtile = app.loadImage('Images/tiles.png')
+    app.tile = app.scaleImage(app.ogtile, 1/3)
     app.multiplier = 0 # 48
     app.position = app.width
 
 def redrawAll(app, canvas):
-    canvas.create_image(24/2 + 24, app.height/4,
+    for i in range(int(app.width/24)):
+        #upper
+        canvas.create_image(24/2 + 24*i, app.height/4,
                             image=ImageTk.PhotoImage(app.tile))
-    canvas.create_image(24/2 + 24, 3 * app.height/4,
+        
+        #lower
+        canvas.create_image(24/2 + 24*i, 3 * app.height/4,
                             image=ImageTk.PhotoImage(app.tile))
         
     # for i in range(int(app.multiplier/2)):
