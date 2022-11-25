a0f2d9c36ddac2681a78b8a4668784a3bb2d1eeb
lakshmi-adiga <ladiga@andrew.cmu.edu>
2022-11-24 21:58:42 -0500

    did oop for player! Now Imma make him jump and finish backtracking before 4 tmrw

--- classes.py
+++ classes.py
@@ -1,40 +1,12 @@
 from cmu_112_graphics import *
+import classes
+classes.Player()
 
+
+    
 # #make a player class to be integrated into the rest of the code
 
 # def appStarted(app):
-#     app.timerDelay = 50
-    
-#     #background
-#     app.bg = app.loadImage("Images/bg1.png")
-#     app.posX1 = app.width/2
-#     app.posX2 = app.width/2 * 3
-#     app.scrollX = 0
-    
-#     #terrain
-#     app.ogtiles = app.loadImage("Images/tiles.png")
-#     app.tiles = app.scaleImage(app.ogtiles, 1/3)
-#     app.xpos = 12
-    
-#     #character running animation
-#     app.ogcharacterstrip = app.loadImage("Images/running.png")
-#     app.characterstrip = app.scaleImage(app.ogcharacterstrip, 1/2) #52
-#     app.characters = []
-
-#     for i in range(6):
-#         character = app.characterstrip.crop((i*55, 0, 55*(i+1), 65))
-#         app.characters.append(character)
-    
-#     app.charcopy = copy.copy(app.characters)
-    
-#     for i in range(6):
-#         app.characters.append(app.charcopy[5-i])
-        
-#     app.charCounter = 0
-    
-#     #changing in between running and standing
-#     app.isRunning = False
-    
 #     app.ogstanding = app.loadImage("Images/standing.png")
 #     app.standing = app.scaleImage(app.ogstanding, 1/2)
 
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from cmu_112_graphics import *
-from classes import *
+
+class Player():
+    def __init__(self, xpos, ypos, chars):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.chars = chars
+        self.image = self.chars[0] 
+    
+    def isStanding(self):
+        self.image = self.chars[0]
+    
+    def isRunning(self, dx, frame):
+        charsList = copy.copy(self.chars)
+        charsList.pop(0)
+        self.xpos += dx
+        self.image = charsList[frame]
+    
+    def isJumping(self, direction, dx):
+        pass
 
 def appStarted(app):
     app.timerDelay = 1
-    
+    app.timerCounter = 0
     #background
     app.bg = app.loadImage("Images/bg1.png")
     app.posX1 = app.width/2
@@ -17,22 +35,28 @@ def appStarted(app):
     
     #creation of the app.chars list
     app.ogcharstrip = app.loadImage("Images/running.png")
-    app.charstrip = app.scaleImage(app.ogcharstrip, 1/2) 
-    app.chars = []
-
+    app.charstrip = app.scaleImage(app.ogcharstrip, 1/2)
+    app.ogstanding = app.loadImage("Images/standing.png")
+    app.standing = app.scaleImage(app.ogstanding, 1/2) 
+    app.charrunning = [app.standing]
     for i in range(6):
         char = app.charstrip.crop((i*55, 0, 55*(i+1), 65))
-        app.chars.append(char)
-    
-    app.charcopy = copy.copy(app.chars)
-    
+        app.charrunning.append(char)
+    app.charcopy = copy.copy(app.charrunning)
     for i in range(6):
-        app.chars.append(app.charcopy[5-i])
-        
-    app.charCounter = 0
-    app.isRunning = False
+        app.charrunning.append(app.charcopy[5-i])    
+    
+    #!app.charrunning is the list of character frames for running animation
+    
+    #player
+    app.player = Player(app.width/8, app.height*4/5 - 40, app.charrunning)
+    app.running = False
+    app.charCounter = 0   
+    
     
 def timerFired(app):
+    app.timerCounter += 1
+    
     #background 
     app.scrollX += 10
     if app.width/2 + app.posX1 - app.scrollX <= 0:
@@ -44,6 +68,18 @@ def timerFired(app):
     if app.xpos + 24 * (int(app.width/24)-1) - app.scrollX <= 0:
         app.xpos += app.width
     
+    #player
+    #! the variable dx is initialized here, the first variable of app.player.isRunning
+    if app.running == True:
+        app.player.isRunning(10.5, app.timerCounter % 6)
+    if app.running == False:
+        app.player.isStanding()
+    
+def keyPressed(app, event):
+    if (event.key == "Right"):
+        app.running = True
+    if (event.key == "Down"):
+        app.running = False
     
 def redrawAll(app, canvas):
     #background
@@ -57,5 +93,8 @@ def redrawAll(app, canvas):
         canvas.create_image(xpos, app.height * 4/5, image = ImageTk.PhotoImage(app.tiles))
         canvas.create_image(app.width - 12 + xpos, app.height/4, image = ImageTk.PhotoImage(app.tiles))
         canvas.create_image(app.width - 12 + xpos, app.height * 4/5, image = ImageTk.PhotoImage(app.tiles))
+    
+    #player
+    canvas.create_image(app.player.xpos - app.scrollX, app.player.ypos, image = ImageTk.PhotoImage(app.player.image))
      
 runApp(width = 1500, height = 532)
