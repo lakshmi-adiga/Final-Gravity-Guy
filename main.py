ec30f3053a4a2302f7bfd45f484adf68a23f0b64
lakshmi-adiga <ladiga@andrew.cmu.edu>
2022-11-25 19:56:53 -0500

    im fucking cracked i invented physics

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 from cmu_112_graphics import *
 
 class Player():
-    def __init__(self, xpos, ypos, chars):
+    def __init__(self, xpos, ypos, charrunning, charjumping, upsidedownrun):
         self.xpos = xpos
         self.ypos = ypos
-        self.chars = chars
-        self.image = self.chars[0] 
+        self.charrunning = charrunning
+        self.charjumping = charjumping
+        self.upsidedownrun = upsidedownrun
+        self.image = self.charrunning[0] 
     
     def isStanding(self):
-        self.image = self.chars[0]
+        self.image = self.charrunning[0]
     
     def isRunning(self, dx, frame):
-        charsList = copy.copy(self.chars)
+        charsList = copy.copy(self.charrunning)
         charsList.pop(0)
         self.xpos += dx
         self.image = charsList[frame]
     
-    def isJumping(self, direction, dx):
-        pass
+    def upsideDownRunning(self, dx, frame):
+        charsList = copy.copy(self.upsidedownrun)
+        charsList.pop(0)
+        self.xpos += dx
+        self.image = charsList[frame]
+    
+    def isJumping(self, dx, frame):
+        self.xpos += dx*1.3
+        if self.ypos >= 187:
+            self.ypos -= 38
+            self.image = self.charjumping[frame]
+            if self.image == self.charjumping[-2]:
+                self.image = self.charjumping[-1]
+    
+    def isDownJump(self, dx, frame):
+        self.xpos += dx*1.3
+        if self.ypos < 385:
+            self.ypos += 38
+            self.image = self.charjumping[frame]
 
 def appStarted(app):
     app.timerDelay = 1
@@ -33,7 +52,7 @@ def appStarted(app):
     app.tiles = app.scaleImage(app.ogtiles, 1/3)
     app.xpos = 12
     
-    #creation of the app.chars list
+    #creation of the app.charrunning list for running animation
     app.ogcharstrip = app.loadImage("Images/running.png")
     app.charstrip = app.scaleImage(app.ogcharstrip, 1/2)
     app.ogstanding = app.loadImage("Images/standing.png")
@@ -47,11 +66,38 @@ def appStarted(app):
         app.charrunning.append(app.charcopy[5-i])    
     
     #!app.charrunning is the list of character frames for running animation
+
+    #creation of app.charjumping list for jumping animation
+    app.ogjumplist = app.loadImage("Images/startjump.png")
+    app.jumplist = app.scaleImage(app.ogjumplist, 1/2)
+    app.charjumping = []
+    for i in range(3):
+        char = app.charstrip.crop((i*55, 0, 55*(i+1), 65))
+        app.charjumping.append(char)
+    app.ogmidjump = app.loadImage("Images/midjump.png")
+    app.midjump = app.scaleImage(app.ogmidjump, 1/2)
+    app.ogmidjump2 = app.loadImage("Images/midjump2.png")
+    app.midjump2 = app.scaleImage(app.ogmidjump2, 1/2)
+    app.oglast = app.loadImage("Images/lastjumpframe.png")
+    app.last = app.scaleImage(app.oglast, 1/2)
+    app.down = app.standing.transpose(Image.FLIP_TOP_BOTTOM)
+    app.charjumping.extend([app.midjump, app.midjump2, app.last, app.down])
     
+    #! app.charjumping is the list of characters for the jumping animation, including app.standing
+    
+    #creation of app.upsidedownrunning which is the character running upside down
+    app.upsidedownrunning = []
+    for frame in app.charrunning:
+        insertframe = frame.transpose(Image.FLIP_TOP_BOTTOM)
+        app.upsidedownrunning.append(insertframe)
+      
     #player
-    app.player = Player(app.width/8, app.height*4/5 - 40, app.charrunning)
+    app.player = Player(app.width/2, app.height*4/5 - 40, app.charrunning, app.charjumping, app.upsidedownrunning)
+    app.standing = True
     app.running = False
-    app.charCounter = 0   
+    app.jumping = False
+    app.isAbove = False
+    app.isBelow = True
     
     
 def timerFired(app):
@@ -70,16 +116,41 @@ def timerFired(app):
     
     #player
     #! the variable dx is initialized here, the first variable of app.player.isRunning
-    if app.running == True:
+    if app.running == True and app.isBelow == True:
         app.player.isRunning(10.5, app.timerCounter % 6)
-    if app.running == False:
+    if app.running == True and app.isAbove == True:
+        app.player.upsideDownRunning(10.5, app.timerCounter % 6)
+    if app.standing == True and app.isBelow == True:
         app.player.isStanding()
-    
+    if app.standing == True and app.isAbove == True:
+        app.player.image = app.down
+    if app.jumping == True and app.isBelow == True:
+        if app.player.ypos >= 187:
+            app.player.isJumping(10.5, app.timerCounter % 6)
+        if app.player.ypos <= 187:
+            app.jumping = False
+            app.isBelow = False
+            app.isAbove = True
+    if app.jumping == True and app.isAbove == True:
+        if app.player.ypos < 385:
+            app.player.isDownJump(10.5, -1 * app.timerCounter % 6)
+        if app.player.ypos >= 385:
+            app.jumping = False
+            app.isBelow = True
+            app.isAbove = False
+
 def keyPressed(app, event):
     if (event.key == "Right"):
+        app.standing = False
         app.running = True
-    if (event.key == "Down"):
+    if (event.key == "Up"):
+        app.standing = True
         app.running = False
+        app.jumping = True
+    if (event.key == "Down"):
+        app.standing = True
+        app.running = False
+        app.jumping = True
     
 def redrawAll(app, canvas):
     #background
